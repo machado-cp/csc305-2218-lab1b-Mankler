259b001c95821a298fa79ee08cd4a2413ccb8ade
Mankler <matt_rey@me.com>
2021-09-20 23:06:24 -0700

    Added functionality to addAlternatives function. If no word exists in map, adds it. If word does exist, streams are used to create a unique list of every synonym in alphabetical order. Also completed all CheckStyle and SonarLint tasks.

--- src/edu/calpoly/csc305/debugging/DebuggerMain.java
+++ src/edu/calpoly/csc305/debugging/DebuggerMain.java
@@ -1,12 +1,22 @@
 package edu.calpoly.csc305.debugging;
 
 public class DebuggerMain {
+  /**
+   * runs the thesaurus functions.
+   *
+   * @param args Contains program arguments.
+   *
+   */
   public static void main(String[] args) {
     Thesaurus thesaurus = new Thesaurus();
 
-    thesaurus.addAlternatives("interesting",
-         "absorbing", "consuming", "engaging", "fascinating");
+    String interesting = "interesting";
 
-    System.out.println(thesaurus.alternatives("interesting"));
+    thesaurus.addAlternatives(interesting,
+         "absorbing", "consuming", "engaging", "fascinating");
+    System.out.println(thesaurus.alternatives(interesting));
+    thesaurus.addAlternatives(interesting, "absorbing", "cool", "fascinating", "sick");
+
+    System.out.println(thesaurus.alternatives(interesting));
   }
 }
--- src/edu/calpoly/csc305/debugging/Thesaurus.java
+++ src/edu/calpoly/csc305/debugging/Thesaurus.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Thesaurus {
   private Map<String, List<String>> synonyms;
@@ -12,8 +13,29 @@ public class Thesaurus {
     synonyms = new HashMap<>();
   }
 
+  /**
+   * If the word already maps to a list, then that list is extended.
+   * Otherwise a new list is added to the map.
+   *
+   * @param word Contains word that will be entered into thesaurus.
+   *
+   * @param alternatives Contains an array of synonyms to word.
+   *
+   */
   public void addAlternatives(String word, String ... alternatives) {
-    synonyms.get(word).addAll(Arrays.asList(alternatives));
+
+    if (synonyms.containsKey(word)) {
+
+      synonyms.replace(word, Stream.concat(synonyms.get(word)
+          .stream()
+          .filter(w -> !Arrays.asList(alternatives).contains(w)), Arrays.stream(alternatives)
+        )
+          .sorted()
+          .toList()
+      );
+    } else {
+      synonyms.put(word, Arrays.asList(alternatives));
+    }
   }
 
   public List<String> alternatives(String word) {
